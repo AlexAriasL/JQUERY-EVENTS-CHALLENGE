0552f0a6058a1bf8c5f99c29f39d82dd474b1594
AlexAriasL <alex.arias.l@gmail.com>
2019-11-15 20:45:07 +0000

    Jquery effects challenge

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
-    $("#button_effects1").click(function(){
-            $('#button_effects1').hide('slow');
 
+    /* This jQuery effect hides the button with ID button_effects1
+    $("#button1").click(function(){
+            $(this).hide('slow');
+       */ 
         });
+
+    $("#button1").click(function(){
+            $("#para1").slideToggle('slow', function(){
+       });
 }); 
